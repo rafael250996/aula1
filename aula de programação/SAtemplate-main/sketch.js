e3e5b34bb1d858936abfdc133ebffb52439dd940
rafael250996 <107442934+rafael250996@users.noreply.github.com>
2022-06-13 19:51:53 -0300

    Add files via upload

---
+++ aula de programação/SAtemplate-main/sketch.js
@@ -0,0 +1,11 @@
+var nome,cidade,idade
+
+nome="rafael ribeiro"
+cidade="Não-Me-Toque"
+idade="14 anos"
+
+console.log("hello");
+console.log("tudo bom?")
+console.log(nome)
+console.log(cidade)
+console.log(idade)
